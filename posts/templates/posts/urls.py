27af58c7a7a20b8014da5401cf16dc917fce2087
david1234-4 <okenydavidojok7@gmail.com>
2026-01-26 20:59:21 +0300

    Remove old Django project files

--- posts/templates/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.posts_list),
-    path('courses/',views.courses),
-    path('profiles/',views.profiles),
-    path('downloads/',views.downloads),
-]
